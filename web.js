21e22c0bcbf468c04df14e2c1a4b087a923978fc
Ubuntu <ubuntu@ip-172-31-18-233.us-west-2.compute.internal>
2013-07-22 20:04:21 +0000

    reverting

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
-var buff = buf.toString(fs.readFileSync('index.html'));
-console.log(buff);
+// var buff = buf.toString(fs.readFileSync('index.html'));
+// console.log(buff);
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(fs.readFileSync('./bistarter/index.html'));
+    response.send("hello world 3!");
 });
 
 var port = process.env.PORT || 5000;
